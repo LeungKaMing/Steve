7430e3f7f258e9b44befae5324198a5a9551eee0
leungkaming <510314588@qq.com>
2018-07-20 18:29:21 +0800

    feat: init

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const {exec} = require('child_process')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    process.argv.slice(2).forEach((arg) => {
+        arg = arg.replace(/-/g, '')
+        switch (arg) {
+            case 'demo':
+                // 读取文件
+                let result = ''
+                const readable = fs.createReadStream('./template/index.html')
+                readable.on('data', (chunk) => {
+                    result += chunk
+                });
+                readable.on('end', () => {
+                    res.end(result)
+                });
+                break
+            case 'cp':
+                // 子进程
+                exec('node ./demo.js', (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    console.log(`stderr: ${stderr}`);
+                })
+                break
+            case 'vue':
+                exec('webpack', (error, stdout, stderr) => {
+                    console.log(`stdout: ${error}`);
+                    console.log(`stderr: ${stdout}`);
+                    console.log(`stderr: ${stderr}`);
+                })
+                break
+            default:
+                res.end('Hello World!')
+        }
+    })
+})
+
+server.listen(8080, () => {
+    console.log('启动服务，端口为8080')
+})
