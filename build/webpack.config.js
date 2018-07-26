d351f7036e81c5d6859b5fb709b5f4ec552935dd
leungkaming <510314588@qq.com>
2018-07-26 20:52:51 +0800

    feat: 0726修改webpack配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   }
