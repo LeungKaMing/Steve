7627baac8b552cf31e4164d11b58ee3d77f0f462
LeungKaMing <510314588@qq.com>
2018-08-16 02:02:51 +0800

    feat: 0816;:sparkles:src/app.js增加vue.html路由;

--- src/app.js
+++ src/app.js
@@ -88,6 +88,16 @@ const server = http.createServer(async (req, res) => {
                     res.writeHeader(200, {'Content-Type':'text/html;charset=UTF-8'})
                     res.end(result)
                 });
+            } else if (req.url === '/vue.html') {
+                let result = ''
+                const readStream = fs.createReadStream('../dist/vue.html')
+                readStream.on('data', (chunk) => {
+                    result += chunk
+                });
+                readStream.on('end', () => {
+                    res.writeHeader(200, {'Content-Type':'text/html;charset=UTF-8'})
+                    res.end(result)
+                });
             } else if (req.url === '/favicon.ico') {
                 res.end('favicon.ico')
             } else {
