23732a66a8b1fe1b90f8ee57fd470563df3f790c
LeungKaMing <510314588@qq.com>
2018-08-25 00:15:36 +0800

    feat: 0824:memo:补充注释

--- src/components/vue/IntroComp.vue
+++ src/components/vue/IntroComp.vue
@@ -52,6 +52,10 @@
                     msg
                 })
             },
+            /**
+             * Action 提交的是 mutation，而不是直接变更状态。
+             * Action 可以包含任意异步操作。
+             */
             ...mapActions({
                 text: 'delayText'
             }),
