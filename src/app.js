e927363d5ff8da846725c46e286315bca2e3c543
leungkaming <510314588@qq.com>
2018-11-14 14:37:24 +0800

    feat: :wrench:1114增加页面缓存处理

--- src/app.js
+++ src/app.js
@@ -7,6 +7,47 @@ const { URL } = require('url'); // 用于url处理与解析
 // 已经实现了类似webpack的插件clean-webpack-plugin功能
 const rm = require('./pkg/rm')
 
+// 第三方插件
+/**
+ * 性能
+ * ssr可以提高首屏加载的速度，减少白屏时间，通过以下设置可以提高性能，减少服务器资源的占用，加快访问速度。
+ * （1）页面级别的缓存 将渲染完成的页面缓存到内存中，同时设置最大缓存数量和缓存时间。 优势：大幅度提高页面的访问速度 代价：增加服务器内存的使用
+ * （2）接口级别的缓存，如下：
+    // 适用：通用性强的接口
+    // 将通用的接口缓存到内存，减少服务端接口请求的时间
+    import axios from 'axios'
+    import qs from 'qs'
+    import md5 from 'md5'
+
+    const LRU = require('lru-cache');//删除最近最少使用条目的缓存对象
+    const microCache = LRU({
+        max: 100,//最大存储100条
+        maxAge: 1000 // 存储在 1 秒后过期
+    })
+
+    export default {
+        get(url, data) {
+            //通过 url 和参数, 生成一个唯一的 key
+            const key = md5(url + JSON.stringify(data))
+            //如果命中缓存则返回缓存
+            if (microCache.has(key)) {
+                return Promise.resolve(microCache.get(key))
+            }
+            return axios.get(url,{params:data}).then(res => {
+                //如果需要缓存，则缓存
+                if (data.cache) microCache.set(key, res)
+                return res
+            })
+        }
+    }
+ */
+
+const LRU = require('lru-cache');//删除最近最少使用条目的缓存对象
+let microCache = LRU({
+    max: 100,//最大存储100条
+    maxAge: 5000 // 存储在 5 秒后过期, 保证5秒内刷新如果该对象存在键，都会从拿该键值
+}) // 实例化配置缓存对象
+
 let webpackBundleResult = false
 
 // 服务 启动/重启 都会执行【删除dist目录】
@@ -117,44 +158,54 @@ const server = http.createServer(async (req, res) => {
             } else if (/\/vuessr/.test(req.url)) {
                 const isServerBundle = path.join(__dirname, '../dist/assets/vue-ssr-server-bundle.json')
                 const isClientBundle = path.join(__dirname, '../dist/assets/vue-ssr-client-manifest.json')
-                if (!!fs.existsSync(isServerBundle) && !!fs.existsSync(isClientBundle)) {
-                    const {createBundleRenderer} = require('vue-server-renderer')
-                    const serverBundle = require(path.join(__dirname, '../dist/assets/vue-ssr-server-bundle.json'))
-                    const clientManifest = require(path.join(__dirname, '../dist/assets/vue-ssr-client-manifest.json'))
-                    let renderer = createBundleRenderer(serverBundle, {
-                        runInNewContext: false,
-                        template: fs.readFileSync(path.join(__dirname, './template/vueSSR.html'), 'utf-8'),
-                        clientManifest
-                    })
-                    const orignUrl = 'http://' + req.headers.host + req.url
-                    // console.log('>>>>>>>>', orignUrl, req.url)
-                    // 渲染上下文对象 => 写法与vue保持一致
-                    const templateContext = {
-                        title: 'vue ssr demo',
-                        meta: `
-                            <meta charset="UTF-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                        `,
-                        url: new URL(orignUrl),
-                        tempUrl: req.url
-                    }
-                    renderer.renderToString(templateContext, (err, html) => {
-                        if (err) {
-                            console.log(err, '<<<<<')
-                            if (err.code === 404) {
-                                res.statusCode = 404
-                                res.end('Page not found')
-                            } else {
-                                res.statusCode = 500
-                                res.end('SSR Internal Server Error')
-                            }
-                        } else {
-                            res.end(html)
-                        }
-                    })
+                // （1）页面级别的缓存
+                const hit = microCache.get(req.url)
+                // console.log(hit, '<<<<<<<<第一次hit肯定为undefined，访问后hit就会有值了')
+                if (hit) {
+                    return res.end(hit)
                 } else {
-                    res.end('ssr??')
+                    if (!!fs.existsSync(isServerBundle) && !!fs.existsSync(isClientBundle)) {
+                        const {createBundleRenderer} = require('vue-server-renderer')
+                        const serverBundle = require(path.join(__dirname, '../dist/assets/vue-ssr-server-bundle.json'))
+                        const clientManifest = require(path.join(__dirname, '../dist/assets/vue-ssr-client-manifest.json'))
+                        let renderer = createBundleRenderer(serverBundle, {
+                            runInNewContext: false,
+                            template: fs.readFileSync(path.join(__dirname, './template/vueSSR.html'), 'utf-8'),
+                            clientManifest
+                        })
+                        const orignUrl = 'http://' + req.headers.host + req.url
+                        // console.log('>>>>>>>>', orignUrl, req.url)
+                        // 渲染上下文对象 => 写法与vue保持一致
+                        const templateContext = {
+                            title: 'vue ssr demo',
+                            meta: `
+                                <meta charset="UTF-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                            `,
+                            url: new URL(orignUrl),
+                            tempUrl: req.url
+                        }
+                        renderer.renderToString(templateContext, (err, html) => {
+                            if (err) {
+                                console.log(err, '<<<<<')
+                                if (err.code === 404) {
+                                    res.statusCode = 404
+                                    res.end('Page not found')
+                                } else {
+                                    res.statusCode = 500
+                                    res.end('SSR Internal Server Error')
+                                }
+                            } else {
+                                res.end(html)
+                                //将页面缓存到缓存对象中
+                                microCache.set(req.url, html)
+                            }
+                        })
+                    } else {
+                        res.statusCode = 500
+                        res.end('SSR Internal Server Error')
+                    }
                 }
             } else if (/\/react.html/.test(req.url)) {
                 let result = ''
